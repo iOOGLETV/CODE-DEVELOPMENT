754396b67a4091fa14abef3cb0d4604fcaf9e999
Big_O  Project Portfolio <omarmsalih@gmail.com>
2018-05-28 03:35:30 -0500

    Add files via upload

---
+++ JAVA/JAVA Code Portfolio/JAVA 2/ATM 4 JAVA 2/ATM 4/src/atm2/ATM.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm2;
+import java.io.*;
+import java.util.*;
+/**
+ * @author 
+ */
+public class ATM 
+{
+    int numberOfAccounts = 3;
+    private Account[] myAccounts = new Account[numberOfAccounts];    
+    
+    
+    public static void main(String[] args) throws Exception
+    {
+        ATM A = new ATM();
+        A.loadAccounts();
+        A.greeting();
+        A.runATM();
+    }
+    
+    void greeting()
+    {       
+        Scanner sc= new Scanner(System.in);
+//        System.out.println("\nWelcome, is this your first time using the ATM? (Y/N)");
+//        String input = sc.next();
+        System.out.println( "|==================================|");
+        System.out.println( "|        FIRST NATIONAL BANK       |");
+        System.out.println( "|***********Menu Options***********|");
+        System.out.println( "|__________________________________|");
+        System.out.println( "|       Come in and ask about      |");
+        System.out.println( "|           our 5% earned          |");
+        System.out.println( "|          on your all your        |");
+        System.out.println( "|     Checking/Saving balances!    |");
+        System.out.println( "|__________________________________|");
+        System.out.println( "|   Please Make Selection Now...   |");
+        System.out.println( "|==================================|");  
+        System.out.println("\n\n");
+        System.out.print("Are you a new customer (Y or N)?   ");
+        String input = sc.next();
+        
+        if(input.equalsIgnoreCase("y"))
+        {
+        populateAcct();
+        }
+        else if(input.equalsIgnoreCase("n"))
+        {
+        runATM();
+        }
+        else
+        {            
+        System.out.println("\nPlese type in Y or N, either case is acceptable\n");
+        greeting();
+        }
+    }
+    
+    void greeting2()
+    {       
+        Scanner sc= new Scanner(System.in);
+        runATM();
+        greeting2();
+    }
+    
+    void runATM() 
+    {
+        try
+        {
+           saveAccounts();
+           selectAcct();
+        }   
+        catch(NullPointerException exc)
+        {
+            System.out.println("\nInvalid Input\n");    
+        }   
+        saveAccounts();
+        greeting();
+        runATM();      
+    }
+    
+    void populateAcct()
+    {
+        Scanner sc = new Scanner(System.in);
+        int index = -99;
+        for(int i = 0; i<myAccounts.length; i++)
+        {
+            if(myAccounts[i] == null)
+            {
+               index = i;
+               break;
+            }
+        }
+        if(index !=-99)
+        {
+           System.out.println("\nPlease enter a name to be associated with the type of account you want");
+           String name = sc.nextLine();
+           for(int i = 0; i<myAccounts.length; i++)
+           {
+               if(myAccounts[i] != null && name.equalsIgnoreCase(myAccounts[i].getName()))
+               {
+                   System.out.println("\nThat name has already been used\n");
+                   greeting2();
+                   return;                
+               }
+           }                  
+           System.out.println("\nPlease enter a PIN to use with this account");
+           String PIN = sc.nextLine();
+           System.out.println("\nWhat type of account would you like to open, checking, savings, or super saver?");
+           String type = sc.nextLine();
+
+           if(type.equalsIgnoreCase("checking"))
+           {
+               myAccounts[index] = new Checking(100, index, name, PIN);
+               System.out.println("\nBalances start at $100, and the annual interest rate is 5%");
+               greeting();
+           }   
+           else if(type.equalsIgnoreCase("savings"))
+           {
+               myAccounts[index] = new Savings(100, index, name, PIN);
+               System.out.println("\nBalances start at $100, and the annual interest rate is 30%");
+               greeting();
+           }
+           else if(type.equalsIgnoreCase("super saver"))
+           {
+               myAccounts[index] = new SuperSaver(100, index, name, PIN);
+               System.out.println("\nBalances start at $100, and the annual interest rate is 50%");
+               System.out.println("In a super saver account, you can only make 12 withdrawls a year");
+               greeting();
+           }
+           else
+           {
+               System.out.println("\nInvalid account type");
+               populateAcct();
+           }
+        }         
+        else
+        {
+            System.out.println("\nSorry, all availible accounts are full\n");
+            greeting();
+        }    
+    }            
+    void selectAcct()
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = -99;
+        String name, PIN;
+        
+        try
+        {
+           System.out.println("\nEnter your account name");
+           name = sc.nextLine();
+           
+           System.out.println("\nEnter your PIN");
+           PIN = sc.nextLine();
+           
+           for(int i = 0; i<myAccounts.length; i++)
+           {
+               if(myAccounts[i] != null && name.equalsIgnoreCase(myAccounts[i].getName()) && PIN.equalsIgnoreCase(myAccounts[i].getPIN()))
+               {
+                 num = i;
+               }
+           }  
+           System.out.println();
+           myAccounts[num].Menu();
+        }
+        catch(ArrayIndexOutOfBoundsException exc)
+        {
+             System.out.println("\nInvalid Input");
+             greeting();
+        } 
+    }   
+    
+    public void loadAccounts()
+    {
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("file1.out")))
+        {
+            myAccounts = (Account[]) ois.readObject();
+        }
+        catch(IOException | ClassNotFoundException e) 
+        {
+            myAccounts = new Account[numberOfAccounts]; 
+        }
+    }
+    
+    public void saveAccounts()
+    {
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("file1.out")))
+        {
+            oos.writeObject(myAccounts);
+            oos.flush();
+        }
+        catch (Throwable e)
+	{
+            System.err.println(e);
+        }
+    }
+}
